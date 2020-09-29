93b7250a5a5af0b53f18b5995e7ac3b223fa6a68
vyacheslav <vyachesl.n@gmail.com>
2020-09-29 23:03:27 +0600

    M1-T3-A1

---
+++ src/components/app/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page';
+
+const App = (props) => {
+  const {moviePoster} = props;
+
+  return (
+    <MainPage
+      {...{moviePoster}}
+    />
+  );
+};
+
+App.propTypes = {
+  moviePoster: PropTypes.object.isRequired
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/components/app/app';
 
+const MoviePoster = {
+  TITLE: `The Grand Budapest Hotel`,
+  GENRE: `Drama`,
+  YEAR: `2014`
+};
+
+ReactDOM.render(
+    <App
+      moviePoster = {MoviePoster}
+    />,
+    document.querySelector(`#root`)
+);
