9c83687033cbcf3053afe405d04cac66f6e4c2c4
vyacheslav <vyachesl.n@gmail.com>
2020-10-29 17:59:10 +0600

    M5-T1 Добавляет Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import films from "./mocks/films";
 import reviews from "./mocks/reviews";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {reducer} from "./store/reducer";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 const movieCard = {
   title: `The Grand Budapest Hotel`,
@@ -11,10 +19,12 @@ const movieCard = {
 };
 
 ReactDOM.render(
+  <Provider store={store}>
     <App
       movieCard={movieCard}
       films={films}
       reviews={reviews}
-    />,
-    document.querySelector(`#root`)
+    />
+  </Provider>,
+  document.querySelector(`#root`)
 );
