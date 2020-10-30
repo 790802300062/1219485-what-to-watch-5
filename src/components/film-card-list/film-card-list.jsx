22c5c48127d5fb026fb1ee048ec05e7c89ae679e
vyacheslav <vyachesl.n@gmail.com>
2020-10-30 10:14:28 +0600

    Выносит кнопку Show More

--- src/components/film-card-list/film-card-list.jsx
+++ src/components/film-card-list/film-card-list.jsx
@@ -2,6 +2,9 @@ import React, {PureComponent} from "react";
 import {FilmTypes} from "../../prop-types-validations";
 import FilmCard from "../film-card/film-card";
 import withVideoPlayer from "../../hocs/with-video-player";
+import ShowMoreButton from "../show-more-button/show-more-button";
+
+const FILMS_AMOUNT_PER_STEP = 8;
 
 const FilmCardWrapped = withVideoPlayer(FilmCard);
 
@@ -10,22 +13,27 @@ class FilmCardList extends PureComponent {
     super(props);
 
     this.state = {
-      activeCard: null,
+      shownCards: Math.min(FILMS_AMOUNT_PER_STEP, this.props.films.length),
     };
   }
 
   render() {
     const {films} = this.props;
+    const {shownCards} = this.state;
+    const shownFilms = films.slice(0, shownCards);
 
     return (
-      <div className="catalog__movies-list">
-        {films.map((film, i) => (
-          <FilmCardWrapped
-            key={`film-${i}`}
-            film={film}
-          />
-        ))}
-      </div>
+      <>
+        <div className="catalog__movies-list">
+          {shownFilms.map((film, i) => (
+            <FilmCardWrapped
+              key={`film-${i}`}
+              film={film}
+            />
+          ))}
+        </div>
+        {shownFilms.length < films.length ? <ShowMoreButton /> : null}
+      </>
     );
   }
 }
