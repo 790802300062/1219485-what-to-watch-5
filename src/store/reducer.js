3cbca0d3becaf6e7600b8582067ec7efc96adf5b
vyacheslav <vyachesl.n@gmail.com>
2020-10-30 09:58:37 +0600

    M5-T1-A1

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,31 @@
+import {ALL_GENRES_FILTER} from "../constants";
+import films from "../mocks/films";
+import {extend, getFilmsFilter} from "../utils";
 import {ActionType} from "./actions";
 
 const initialState = {
-  genre: `all`,
-  films: [],
+  activeGenre: ALL_GENRES_FILTER,
+  films,
+  initialFilms: films
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.CHANGE_GENRE:
-      return state;
+      return extend(state, {
+        activeGenre: action.payload,
+      });
     case ActionType.GET_FILMS_BY_GENRE:
-      return state;
+      const filmsFilter = getFilmsFilter(state.initialFilms);
+      const genre = action.payload;
+      const filmsByGenre = filmsFilter[genre](state.initialFilms);
+
+      return extend(state, {
+        films: filmsByGenre,
+      });
     default:
       return state;
   }
 };
+
+export {reducer};
