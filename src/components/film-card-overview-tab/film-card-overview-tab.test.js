28e5e08a34f3865189104dfa20cf00f78b8d3658
vyacheslav <vyachesl.n@gmail.com>
2020-11-20 11:36:19 +0600

    M8-T1-A1 Tests for films-components

---
+++ src/components/film-card-overview-tab/film-card-overview-tab.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {filmListMock} from "../../test-data";
+import FilmCardOverviewTab from "./film-card-overview-tab";
+
+describe(`FilmCardOverviewTab render`, () => {
+  it(`renders FilmCardOverviewTab component with the first mock movie data`, () => {
+    const tree = renderer
+      .create(
+          <FilmCardOverviewTab
+            film={filmListMock[0]}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`renders FilmCardOverviewTab component with another movie data`, () => {
+    const tree = renderer
+      .create(
+          <FilmCardOverviewTab
+            film={filmListMock[5]}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
